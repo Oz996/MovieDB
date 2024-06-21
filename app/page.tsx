19ea82d3bd7275efd0a4d15fc0a8dc9ba92f95c8
Oz996 <ozgun996@hotmail.com>
2024-06-21 13:33:12 +0200

    using media query hook to display a select on carousel section on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,22 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Result } from "@/types";
 import { getAllTrending } from "@/services/all";
 import CarouselCard from "@/components/CarouselCard";
 import { getMovieList } from "@/services/movies";
-import { useIntersectionObserver } from "@uidotdev/usehooks";
+import { useIntersectionObserver, useMediaQuery } from "@uidotdev/usehooks";
 import { getTvShows } from "@/services/tvShows";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Home() {
   const [hasRendered, setHasRendered] = useState(false);
@@ -24,6 +33,7 @@ export default function Home() {
 
   const [shows, setShows] = useState<Result[] | undefined>([]);
   const [showsType, setshowsType] = useState("free");
+  const isMobile = useMediaQuery("only screen and (max-width: 768px)");
 
   const [popularRef, popularEntry] = useIntersectionObserver({
     threshold: 0.5,
@@ -69,14 +79,18 @@ export default function Home() {
     }
   }, [showsType, showsEntry, hasRendered]);
 
+  const handleSelectChange = (type: string) => {
+    setPopularType(type);
+  };
+
   console.log("is yes yes", popularEntry);
 
   return (
     <>
       <section className="pt-24">
-        <div className="w-full h-[8rem] p-10 bg-black flex flex-col justify-center text-white rounded">
-          <h2 className="text-4xl">Welcome.</h2>
-          <p className="text-2xl">
+        <div className="w-full lg:h-[8rem] p-10 bg-black flex flex-col justify-center text-white rounded">
+          <h2 className="text-2xl lg:text-4xl">Welcome.</h2>
+          <p className="text-lg lg:text-2xl max-md:pt-1">
             Millions of movies, TV shows and people to discover. Explore now.
           </p>
         </div>
@@ -130,34 +144,49 @@ export default function Home() {
 
       <section className="pt-12 px-5" ref={popularRef}>
         <Tabs defaultValue="now_playing">
-          <div className="w-full flex items-center gap-5">
+          <div className="w-full flex max-sm:flex-col items-center gap-5">
             <h2 className="text-xl font-semibold">What's Popular</h2>
-            <TabsList>
-              <TabsTrigger
-                value="now_playing"
-                className="text-md"
-                onClick={() => setPopularType("now_playing")}
-              >
-                Now Playing
-              </TabsTrigger>
-              <TabsTrigger
-                value="top_rated"
-                className="text-md"
-                onClick={() => setPopularType("top_rated")}
-              >
-                Top Rated
-              </TabsTrigger>
-              <TabsTrigger
-                value="upcoming"
-                className="text-md"
-                onClick={() => setPopularType("upcoming")}
-              >
-                Upcoming
-              </TabsTrigger>
-            </TabsList>
+            {isMobile ? (
+              <Select value={popularType} onValueChange={handleSelectChange}>
+                <SelectTrigger>
+                  <SelectValue placeholder={popularType} />
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="now_playing">Now Playing</SelectItem>
+                      <SelectItem value="top_rated">Top Rated</SelectItem>
+                      <SelectItem value="upcoming">Upcoming</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </SelectTrigger>
+              </Select>
+            ) : (
+              <TabsList>
+                <TabsTrigger
+                  value="now_playing"
+                  className="text-md"
+                  onClick={() => setPopularType("now_playing")}
+                >
+                  Now Playing
+                </TabsTrigger>
+                <TabsTrigger
+                  value="top_rated"
+                  className="text-md"
+                  onClick={() => setPopularType("top_rated")}
+                >
+                  Top Rated
+                </TabsTrigger>
+                <TabsTrigger
+                  value="upcoming"
+                  className="text-md"
+                  onClick={() => setPopularType("upcoming")}
+                >
+                  Upcoming
+                </TabsTrigger>
+              </TabsList>
+            )}
           </div>
           <TabsContent value="now_playing">
-            <Carousel>
+            <Carousel className="bg-slate-100 rounded-xl pl-2">
               <CarouselContent className="-ml-1">
                 {popular?.map((item) => (
                   <CarouselCard key={item.id} item={item} />
