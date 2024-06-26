a8f7d65b16afb97949e463f44383c5087e65d306
Oz996 <ozgun996@hotmail.com>
2024-06-26 22:43:33 +0200

    added carousel for billed cast

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -4,6 +4,16 @@ import { useEffect, useState } from "react";
 import { Movie as IMovie } from "@/types";
 import "react-circular-progressbar/dist/styles.css";
 import Banner from "@/components/Banner/Banner";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function Movie({ params }: { params: { id: string } }) {
   const [isLoading, setIsLoading] = useState(true);
@@ -23,5 +33,55 @@ export default function Movie({ params }: { params: { id: string } }) {
     fetchMovieDetails();
   }, [params]);
 
-  return <Banner movie={movie!} />;
+  const cast = movie?.credits.cast;
+  const getCastToDisplay = () => {
+    if (!cast) return null;
+    return cast.slice(0, 8);
+  };
+  console.log(getCastToDisplay());
+
+  return (
+    <>
+      <Banner movie={movie!} />
+      <section>
+        <h2 className="text-2xl font-semibold py-5">Top Billed Cast</h2>
+        <Carousel className="rounded-xl pl-2">
+          <CarouselContent className="-ml-1">
+            {getCastToDisplay()?.map((person) => {
+              return (
+                <CarouselItem
+                  key={person.id}
+                  className="pl-1 basis-1/1 sm:basis-1/3 md:basis-1/3 lg:basis-1/6"
+                >
+                  <Link href={""}>
+                    <motion.div>
+                      <div className="space-y-5 border flex flex-col w-[8.95rem] rounded-lg pb-2">
+                        <div>
+                          <Image
+                            className="rounded-t-lg"
+                            src={`https://image.tmdb.org/t/p/w342${person.profile_path}`}
+                            width={140}
+                            height={140}
+                            alt=""
+                          />
+                        </div>
+                        <div className="p-2">
+                          <p className="font-bold max-w-[10rem] line-clamp-2">
+                            {person.name}
+                          </p>
+                          <p>{person.character}</p>
+                        </div>
+                      </div>
+                    </motion.div>
+                  </Link>
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </section>
+    </>
+  );
 }
