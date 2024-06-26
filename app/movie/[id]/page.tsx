8de8e99d1ac8c2d9dce7d09007f980565cdb50c3
Oz996 <ozgun996@hotmail.com>
2024-06-26 23:42:08 +0200

    adjustments to cast carousel

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -53,27 +53,25 @@ export default function Movie({ params }: { params: { id: string } }) {
                   key={person.id}
                   className="pl-1 basis-1/1 sm:basis-1/3 md:basis-1/3 lg:basis-1/6"
                 >
-                  <Link href={""}>
-                    <motion.div>
-                      <div className="space-y-5 border flex flex-col w-[8.95rem] rounded-lg pb-2">
-                        <div>
-                          <Image
-                            className="rounded-t-lg"
-                            src={`https://image.tmdb.org/t/p/w342${person.profile_path}`}
-                            width={140}
-                            height={140}
-                            alt=""
-                          />
-                        </div>
+                  <motion.div>
+                    <div className="space-y-5 border flex flex-col items-center w-[8.95rem] rounded-lg pb-2">
+                      <Link href={""}>
+                        <Image
+                          className="rounded-t-lg"
+                          src={`https://image.tmdb.org/t/p/original${person.profile_path}`}
+                          width={140}
+                          height={140}
+                          alt=""
+                        />
                         <div className="p-2">
                           <p className="font-bold max-w-[10rem] line-clamp-2">
                             {person.name}
                           </p>
                           <p>{person.character}</p>
                         </div>
-                      </div>
-                    </motion.div>
-                  </Link>
+                      </Link>
+                    </div>
+                  </motion.div>
                 </CarouselItem>
               );
             })}
