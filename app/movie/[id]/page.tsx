f9d04aa8b2a7953b0da5bd0dbbc709f82f1f2f43
Oz996 <ozgun996@hotmail.com>
2024-06-27 16:29:48 +0200

    separate component for side content using aside html element

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -5,8 +5,7 @@ import { Movie as IMovie } from "@/types";
 import "react-circular-progressbar/dist/styles.css";
 import Banner from "@/components/Banner/Banner";
 import PersonCarousel from "@/components/Carousels/PersonCarousel";
-import { formatToDollars } from "@/lib/utils";
-import ExternalLinks from "@/components/ExternalLinks";
+import SideContent from "@/components/SideContent";
 
 export default function Movie({ params }: { params: { id: string } }) {
   const [isLoading, setIsLoading] = useState(true);
@@ -33,40 +32,7 @@ export default function Movie({ params }: { params: { id: string } }) {
       <Banner movie={movie!} />
       <section className="flex">
         <PersonCarousel movie={movie!} />
-        <div>
-          <div className="flex flex-col gap-4 pt-12 w-[15rem]">
-            <ExternalLinks movie={movie!} />
-            <div className="flex flex-col gap-1">
-              <p className="font-semibold">Status</p>
-              <p>{movie?.status}</p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <p className="font-semibold">Original Language</p>
-              <p>{movie?.original_language}</p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <p className="font-semibold">Budget</p>
-              <p>{formatToDollars(movie?.budget!)}</p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <p className="font-semibold">Revenue</p>
-              <p>{formatToDollars(movie?.revenue!)}</p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <p className="font-semibold">Keywords</p>
-              <div className="flex flex-wrap gap-2 text-sm">
-                {movie?.keywords.keywords.map((keyword) => (
-                  <div
-                    key={keyword.id}
-                    className="rounded-lg py-1 px-3 bg-gray-200"
-                  >
-                    <p>{keyword.name}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
+        <SideContent movie={movie!} />
       </section>
     </>
   );
---
+++ components/SideContent.tsx
@@ -0,0 +1,46 @@
+import { Movie } from "@/types";
+import ExternalLinks from "./ExternalLinks";
+import { formatToDollars } from "@/lib/utils";
+
+interface props {
+  movie: Movie;
+}
+
+export default function SideContent({ movie }: props) {
+  return (
+    <aside>
+      <div className="flex flex-col gap-4 pt-12 w-[15rem]">
+        <ExternalLinks movie={movie!} />
+        <div className="flex flex-col gap-1">
+          <p className="font-semibold">Status</p>
+          <p>{movie?.status}</p>
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="font-semibold">Original Language</p>
+          <p>{movie?.original_language}</p>
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="font-semibold">Budget</p>
+          <p>{formatToDollars(movie?.budget!)}</p>
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="font-semibold">Revenue</p>
+          <p>{formatToDollars(movie?.revenue!)}</p>
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="font-semibold">Keywords</p>
+          <div className="flex flex-wrap gap-2 text-sm">
+            {movie?.keywords.keywords.map((keyword) => (
+              <div
+                key={keyword.id}
+                className="rounded-lg py-1 px-3 bg-gray-200"
+              >
+                <p>{keyword.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
+}
