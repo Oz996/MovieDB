dc6085087f1e956d4f8d5c13ae58bf4139243a36
Oz996 <ozgun996@hotmail.com>
2024-06-30 02:24:06 +0200

    using fixed for iframe and improvements to close button styling

--- components/TrailerIframe.tsx
+++ components/TrailerIframe.tsx
@@ -14,7 +14,7 @@ export default function TrailerIframe({ play, trailer, handleClose }: props) {
       {play && (
         <motion.div
           key="video-player"
-          className="absolute left-[20rem] top-[5rem] z-40"
+          className="fixed left-[20rem] top-[3.5rem] z-40"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
@@ -23,11 +23,16 @@ export default function TrailerIframe({ play, trailer, handleClose }: props) {
           <div className="fixed inset-0 w-full h-full bg-black/80" />
           <div className="relative top-0 py-2 px-4 flex justify-between items-center w-[1387px] h-[4rem] text-white bg-black z-50">
             <p className="text-lg">Play Trailer</p>
-            <X className="cursor-pointer" onClick={handleClose as any} />
+            <button
+              onClick={handleClose as any}
+              className="flex items-center justify-center p-3 bg-transparent hover:bg-white/20 duration-300 cursor-pointer group rounded-full"
+            >
+              <X size={17} className="group-hover:opacity-70" />
+            </button>
           </div>
           <div className="z-50">
             <iframe
-              className="absolute inset-0"
+              className="fixed left-[20rem] top-[7rem]"
               width="1387"
               height="780"
               src={`https://www.youtube.com/embed/${trailer}?autoplay=1`}
