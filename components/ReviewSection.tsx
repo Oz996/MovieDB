a34159f0165db9abf281c1cdb92c931ba7f6287c
Oz996 <ozgun996@hotmail.com>
2024-07-01 22:20:16 +0200

    improvements to review card and component logic

--- components/ReviewSection.tsx
+++ components/ReviewSection.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Movie, Review } from "@/types";
+import { Movie } from "@/types";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
@@ -14,7 +14,19 @@ interface props {
 export default function ReviewSection({ movie }: props) {
   const [expanded, setExpanded] = useState(false);
   const reviews = movie?.reviews.results;
-  const reviewToDisplay = reviews?.slice(0, 1);
+  const review = reviews?.[0];
+
+  const date = new Date(review?.created_at);
+
+  const formattedDate = date.toLocaleString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  const username = review?.author_details.username ?? "Unknown";
+  const rating = Math.ceil(review?.author_details.rating!) * 10;
+  const image = review?.author_details.avatar_path;
 
   const handleExpandCard = () => {
     setExpanded(true);
@@ -28,78 +40,60 @@ export default function ReviewSection({ movie }: props) {
     <section>
       <h2 className="text-title font-semibold py-5">Reviews</h2>
       {reviews?.length === 0 && <p>No reviews</p>}
-      {reviewToDisplay?.map((review) => {
-        const date = new Date(review.created_at);
-
-        const formattedDate = date.toLocaleString("en-US", {
-          month: "long",
-          day: "numeric",
-          year: "numeric",
-        });
-
-        return (
-          <Card key={review.id} className="mr-6">
-            <CardHeader>
-              <div className="flex gap-5">
-                <div>
-                  <Image
-                    src={handleDisplayImage(
-                      "w45_and_h45_face",
-                      review.author_details.avatar_path
-                    )}
-                    height={50}
-                    width={50}
-                    alt=""
-                    className="rounded-full object-cover"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <h2 className="font-bold text-lg">
-                    A review by {review.author_details.name}
-                  </h2>
-                  {review.author_details.rating !== null && (
-                    <div className="bg-slate-800 text-white p-2 flex gap-1 items-center rounded-lg">
-                      <FaStar /> <p>{review.author_details.rating}</p>
-                    </div>
-                  )}
-                  <p className="text-sm">
-                    Written by{" "}
-                    <span className="font-semibold">
-                      {review.author_details.name}
-                    </span>{" "}
-                    on {formattedDate}
-                  </p>
-                </div>
+      <Card key={review?.id} className="mr-6">
+        <CardHeader>
+          <div className="flex gap-5">
+            <div>
+              <Image
+                src={handleDisplayImage("w45_and_h45_face", image)}
+                height={50}
+                width={50}
+                alt=""
+                className="rounded-full object-cover"
+              />
+            </div>
+            <div className="flex flex-col">
+              <h2 className="font-bold text-lg">A review by {username}</h2>
+              <div className="flex gap-2 text-sm">
+                {rating != null && (
+                  <div className="bg-slate-800 text-white px-3 flex gap-1 items-center rounded-lg">
+                    <FaStar size={10} /> <p>{rating}%</p>
+                  </div>
+                )}
+                <span>
+                  Written by <span className="font-semibold">{username}</span>{" "}
+                  on {formattedDate}
+                </span>
               </div>
-            </CardHeader>
-            <CardContent>
-              {expanded ? (
-                <>
-                  <p>{review.content}</p>
-                  <Button
-                    onClick={handleCloseCard}
-                    className="bg-transparent border-none p-0 hover:bg-transparent text-black"
-                  >
-                    show less
-                    <ChevronUp size={20} />
-                  </Button>
-                </>
-              ) : (
-                <>
-                  <p className="line-clamp-5">{review.content}</p>
-                  <Button
-                    onClick={handleExpandCard}
-                    className="bg-transparent border-none p-0 hover:bg-transparent text-black"
-                  >
-                    show more
-                    <ChevronDown size={20} />
-                  </Button>
-                </>
-              )}
-            </CardContent>
-          </Card>
-        );
-      })}
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          {expanded ? (
+            <>
+              <p>{review?.content}</p>
+              <Button
+                onClick={handleCloseCard}
+                className="bg-transparent border-none p-0 hover:bg-transparent text-black"
+              >
+                show less
+                <ChevronUp size={20} />
+              </Button>
+            </>
+          ) : (
+            <>
+              <p className="line-clamp-5">{review?.content}</p>
+              <Button
+                onClick={handleExpandCard}
+                className="bg-transparent border-none p-0 hover:bg-transparent text-black"
+              >
+                show more
+                <ChevronDown size={20} />
+              </Button>
+            </>
+          )}
+        </CardContent>
+      </Card>
     </section>
   );
 }
