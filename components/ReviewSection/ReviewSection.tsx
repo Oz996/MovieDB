390ca855e63ef657ce17e0d3e18728dbccb1f6a8
Oz996 <ozgun996@hotmail.com>
2024-08-01 11:09:12 +0200

    added logic for reviews pagination

--- components/ReviewSection/ReviewSection.tsx
+++ components/ReviewSection/ReviewSection.tsx
@@ -1,9 +1,17 @@
-import { Movie, TvShow } from "@/types";
+import { Movie, Review, TvShow } from "@/types";
 import { ChevronDown, ChevronUp } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import ReviewCard from "./components/ReviewCard";
-import { AnimatePresence, motion } from "framer-motion";
+import classNames from "classnames";
 
 interface props {
   movie?: Movie;
@@ -12,9 +20,19 @@ interface props {
 
 export default function ReviewSection({ movie, tvShow }: props) {
   const [showAll, setShowAll] = useState(false);
+  const [reviews, setReviews] = useState<Review[]>();
+  const [currentPage, setCurrentPage] = useState(1);
+  const reviewsPerPage = 5;
 
-  const reviews = movie?.reviews?.results || tvShow?.reviews?.results;
-  console.log("rev", reviews);
+  const allReviews = movie?.reviews?.results || tvShow?.reviews?.results;
+  const totalPages = Math.ceil(allReviews?.length! / reviewsPerPage);
+
+  useEffect(() => {
+    const startIndex = (currentPage - 1) * reviewsPerPage;
+    const endIndex = startIndex + reviewsPerPage;
+    setReviews(allReviews?.slice(startIndex, endIndex));
+  }, [currentPage, allReviews]);
+
   const review = reviews?.[0];
 
   const handleShowReviews = () => {
@@ -23,6 +41,30 @@ export default function ReviewSection({ movie, tvShow }: props) {
 
   const handleHideReviews = () => {
     setShowAll(false);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((current) => current + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((current) => current - 1);
+    }
+  };
+
+  const handleClickPage = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const totalArray = () => {
+    const pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
   };
 
   if (reviews?.length === 0)
@@ -48,6 +90,42 @@ export default function ReviewSection({ movie, tvShow }: props) {
             Show Less Reviews
             <ChevronUp size={20} />
           </Button>
+          {allReviews?.length! > 5 && (
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <Button
+                    className="p-0 bg-white text-black hover:bg-white duration-0 cursor-pointer"
+                    onClick={handlePrevPage}
+                    disabled={currentPage === 1}
+                  >
+                    <PaginationPrevious />
+                  </Button>
+                </PaginationItem>
+                {totalArray().map((page) => (
+                  <PaginationItem key={page}>
+                    <PaginationLink
+                      onClick={() => handleClickPage(page)}
+                      className={classNames({
+                        "bg-slate-200": currentPage === page,
+                        "cursor-pointer": currentPage !== page,
+                      })}
+                    >
+                      {page}
+                    </PaginationLink>
+                  </PaginationItem>
+                ))}
+                <PaginationItem>
+                  <Button
+                    className="p-0 bg-white text-black hover:bg-white duration-0 cursor-pointer"
+                    disabled={currentPage === totalPages}
+                  >
+                    <PaginationNext onClick={handleNextPage} />
+                  </Button>
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          )}
         </div>
       ) : (
         <div>
