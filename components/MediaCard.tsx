3efab0376607903e982cb1b8e64ce3cbea890d6d
Oz996 <ozgun996@hotmail.com>
2024-08-04 21:24:37 +0200

    fixed some issues in mediacard

--- components/MediaCard.tsx
+++ components/MediaCard.tsx
@@ -1,5 +1,5 @@
 import { Result } from "@/types";
-import { handleDisplayImage } from "@/lib/utils";
+import { formatDate, handleDisplayImage } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,10 +10,15 @@ interface props {
 export default function MediaCard({ item }: props) {
   const title = item.title || item.name;
   const date = item.release_date || item.first_air_date;
+
+  const getType = () => {
+    if (item.release_date) return "movie";
+    else return "tv";
+  };
   return (
     <Link
       key={item.id}
-      href={`http://localhost:3000/item/${item.id}`}
+      href={`http://localhost:3000/${getType()}/${item.id}`}
       className="border rounded-lg shadow-md w-[11rem]"
     >
       <Image
@@ -26,7 +31,7 @@ export default function MediaCard({ item }: props) {
 
       <div className="flex flex-col gap-1 p-2">
         <p className="font-semibold max-w-[10rem] line-clamp-2">{title}</p>
-        <p className="text-gray-500">{date}</p>
+        <p className="text-gray-500">{formatDate(date!)}</p>
       </div>
     </Link>
   );
