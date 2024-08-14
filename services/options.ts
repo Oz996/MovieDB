6a5ccfc93071ae02a2db0234a4396af6ad47f1ce
Oz996 <ozgun996@hotmail.com>
2024-08-14 18:32:13 +0200

    switching back to env variable after adding a env variable on netlify

--- services/options.ts
+++ services/options.ts
@@ -2,7 +2,7 @@ const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTAyMjFiYzZkNzQyMTc4MWVjN2NmM2E3MjlkMWU2NSIsInN1YiI6IjY0YTA3MDNlYzM5MGM1MDBjYWZmYWRiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W1T2Ad204uX43WIrXDRmfudR8rCdyLWuyVejhEIQOEU`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_TOKEN}`,
   },
 };
 
