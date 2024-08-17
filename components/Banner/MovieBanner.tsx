a93790137f027d23becb74fbafa40f15e74b6e92
Oz996 <ozgun996@hotmail.com>
2024-08-17 04:22:38 +0200

    brought back optional chaining for banners

--- components/Banner/MovieBanner.tsx
+++ components/Banner/MovieBanner.tsx
@@ -31,7 +31,7 @@ export default function MovieBanner({
   const isMobile = useMediaQuery("only screen and (max-width: 768px)");
   const fetchVideos = async () => {
     try {
-      const res = await getMovieVideos(movie.id);
+      const res = await getMovieVideos(movie?.id);
       const trailers = filterByTrailers(res);
       setVideos(trailers);
     } catch (error: any) {
@@ -60,28 +60,28 @@ export default function MovieBanner({
   if (isLoading) return <BannerLoader />;
 
   return (
-    <BannerContainer backdrop_path={movie.backdrop_path}>
+    <BannerContainer backdrop_path={movie?.backdrop_path}>
       <Image
         width={imageSize()}
         height={imageSize()}
-        src={handleDisplayImage("w1280", movie.poster_path)}
+        src={handleDisplayImage("w1280", movie?.poster_path)}
         alt="Movie poster"
         className="z-20 lg:rounded-lg max-sm:object-cover max-sm:w-full max-md:self-center"
       />
       <MovieDetails
-        title={movie.title}
-        release={movie.release_date}
-        genres={movie.genres}
-        runtime={movie.runtime}
-        tagline={movie.tagline}
-        overview={movie.overview}
-        crew={movie.credits.crew}
+        title={movie?.title}
+        release={movie?.release_date}
+        genres={movie?.genres}
+        runtime={movie?.runtime}
+        tagline={movie?.tagline}
+        overview={movie?.overview}
+        crew={movie?.credits.crew}
         rating={Math.ceil(movie.vote_average * 10)}
         handleShowTrailer={handleShowTrailer}
       />
       <TrailerIframe
         play={playTrailer}
-        trailer={videos[0].key}
+        trailer={videos[0]?.key}
         handleClose={handleCloseTrailer}
       />
     </BannerContainer>
--- components/Banner/TvShowBanner.tsx
+++ components/Banner/TvShowBanner.tsx
@@ -31,7 +31,7 @@ export default function TvShowBanner({
   const isMobile = useMediaQuery("only screen and (max-width: 768px)");
   const fetchVideos = async () => {
     try {
-      const res = await getTvShowVideos(tvShow.id);
+      const res = await getTvShowVideos(tvShow?.id);
       const trailers = filterByTrailers(res);
       setVideos(trailers);
     } catch (error: any) {
@@ -60,29 +60,29 @@ export default function TvShowBanner({
   if (isLoading) return <BannerLoader />;
 
   return (
-    <BannerContainer backdrop_path={tvShow.backdrop_path}>
+    <BannerContainer backdrop_path={tvShow?.backdrop_path}>
       <Image
         width={imageSize()}
         height={imageSize()}
-        src={handleDisplayImage("w1280", tvShow.poster_path)}
+        src={handleDisplayImage("w1280", tvShow?.poster_path)}
         alt="Movie poster"
         className="z-20 lg:rounded-lg max-sm:object-cover max-sm:w-full max-md:self-center"
       />
       <TvShowDetails
-        title={tvShow.name}
-        first_air_date={tvShow.first_air_date}
-        last_air_date={tvShow.last_air_date}
-        genres={tvShow.genres}
-        tagline={tvShow.tagline}
-        overview={tvShow.overview}
-        crew={tvShow.credits.crew}
-        rating={Math.ceil(tvShow.vote_average * 10)}
-        in_production={tvShow.in_production}
+        title={tvShow?.name}
+        first_air_date={tvShow?.first_air_date}
+        last_air_date={tvShow?.last_air_date}
+        genres={tvShow?.genres}
+        tagline={tvShow?.tagline}
+        overview={tvShow?.overview}
+        crew={tvShow?.credits.crew}
+        rating={Math.ceil(tvShow?.vote_average * 10)}
+        in_production={tvShow?.in_production}
         handleShowTrailer={handleShowTrailer}
       />
       <TrailerIframe
         play={playTrailer}
-        trailer={videos[0].key}
+        trailer={videos[0]?.key}
         handleClose={handleCloseTrailer}
       />
     </BannerContainer>
